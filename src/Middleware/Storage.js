4036d9e2b35dd9dbf2c91050921921ae09ca2be6
sarci30 <jdavidsartillo>
2024-05-29 16:53:21 -0600

    first commit

--- src/Middleware/Storage.js
+++ src/Middleware/Storage.js
@@ -2,7 +2,7 @@ import multer from "multer"
 
 const guardar = multer.diskStorage({
     destination: (req,file,cb) =>{
-        cb(null,'./public/uploads')
+        cb(null,'./public')
     },
     filename: (req,file,cb) =>{
         if(file !== null){
