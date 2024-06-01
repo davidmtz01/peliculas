d014df944bcabcf6d9f1b965a43c5577cd4b4d71
davidmtz01 <95955080+davidmtz01@users.noreply.github.com>
2024-06-01 13:47:53 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,24 +1,30 @@
-import express from 'express'
-import morgan from 'morgan'
-import cors from 'cors'
-import mongoose from 'mongoose'
-import {URL,DB_HOST,DB_DATABASE,DB_PORT} from './config.js'
-import rutasPelicula from './Routes/Pelicula.routes.js'
-import rutasAuth from './Routes/Auth.routes.js'
-//const conexion = 'mongodb://'+DB_HOST+':'+DB_PORT+'/'+DB_DATABASE
-mongoose.connect(URL).then()
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import { URL, DB_HOST, DB_DATABASE, DB_PORT } from './config.js';
+import rutasPelicula from './Routes/Pelicula.routes.js';
+import rutasAuth from './Routes/Auth.routes.js';
 
-const app = express()
-app.use(cors())
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.static('public'))
-app.use(rutasPelicula)
-app.use(rutasAuth)
+mongoose.connect(URL).then();
 
-app.use( (req,res) => {
-    res.status(404).json({status:false,errors:'Not found'})
-} )
-//app.get('/', (req,res) => { res.send('Hola mundo')})
+const app = express();
 
-export default app
+// Configuración de CORS
+const corsOptions = {
+  origin: 'http://localhost:5173', // Permitir solicitudes desde http://localhost:5173
+  credentials: true // Permitir el envío de cookies en las solicitudes
+};
+app.use(cors(corsOptions));
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.static('public'));
+app.use(rutasPelicula);
+app.use(rutasAuth);
+
+app.use((req, res) => {
+  res.status(404).json({ status: false, errors: 'Not found' });
+});
+
+export default app;
