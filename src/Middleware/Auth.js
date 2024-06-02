e4f1c988a5e9c1294516f3429ab80940a1258fa4
davidmtz01 <95955080+davidmtz01@users.noreply.github.com>
2024-06-01 21:45:48 -0600

    Update Auth.js

--- src/Middleware/Auth.js
+++ src/Middleware/Auth.js
@@ -1,24 +1,20 @@
-import {Router} from 'express'
-import Jwt from 'jsonwebtoken'
-import { JWT_SECRET } from '../config.js'
+import Jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '../config.js';
 
-export const verificar = Router()
-
-verificar.use( (req,res,next) =>{
-    let token = req.headers['x-access-token'] || req.headers['authorization']
-    if(!token){
-        return res.status(401).json({status:false, errors:['No autorizado']})
+export const verificar = (req, res, next) => {
+    let token = req.headers['x-access-token'] || req.headers['authorization'];
+    if (!token) {
+        return res.status(401).json({ status: false, errors: ['No autorizado'] });
     }
-    if(token.startsWith('Bearer')){
-        token = token.slice(7,token.length)
-        Jwt.verify(token, JWT_SECRET, (error,decoded)=>{
-            if(error){
-                return res.status(401).json({status:false,errors:['Token NO valido']})
+    if (token.startsWith('Bearer')) {
+        token = token.slice(7, token.length);
+        Jwt.verify(token, JWT_SECRET, (error, decoded) => {
+            if (error) {
+                return res.status(401).json({ status: false, errors: ['Token NO valido'] });
+            } else {
+                req.decoded = decoded;
+                next();
             }
-            else{
-                req.decoded = decoded
-                next()
-            }
-        })
+        });
     }
-})
+};
