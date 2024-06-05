368a21d1b226a947b2983d1e1f852eb852570197
davidmtz01 <95955080+davidmtz01@users.noreply.github.com>
2024-06-04 23:22:47 -0600

    Update Storage.js

--- src/Middleware/Storage.js
+++ src/Middleware/Storage.js
@@ -2,7 +2,7 @@ import multer from "multer"
 
 const guardar = multer.diskStorage({
     destination: (req,file,cb) =>{
-        cb(null,'./public')
+        cb(null,'./public/uploads')
     },
     filename: (req,file,cb) =>{
         if(file !== null){
